52a68649a372769f7c75f165d4d789ba18b83921
Cristian Buffa <bufface@gmail.com>
2016-08-25 17:33:47 -0300

    Video 7: Crear listas

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    create () {
+      var list = this.store.createRecord("list", {
+        title: this.listTitle
+      })
+
+      list.save();
+    }
+  }
 });
