b189690509470673af5e60b1faa30ba1616bb7cb
Cristian Buffa <bufface@gmail.com>
2016-08-25 18:20:48 -0300

    Video 9: Eliminar lista

--- app/components/trello-list.js
+++ app/components/trello-list.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete () {
+      this.$().fadeOut("normal", () => {
+        this.list.deleteRecord();
+        this.list.save();
+      });
+    }
+  }
 });
